616c6559f822c4c8fd2603d35b7173d944722dc2
Gaurav Arora <aroragaurav9719@gmail.com>
2026-06-23 06:27:43 +0530

    Updated CORS configuration

--- careerpilot-backend/src/main/java/com/careerpilot/config/SecurityConfig.java
+++ careerpilot-backend/src/main/java/com/careerpilot/config/SecurityConfig.java
@@ -55,11 +55,10 @@
             CorsConfiguration configuration =
                     new CorsConfiguration();
 
-            configuration.setAllowedOrigins(
+            configuration.setAllowedOriginPatterns(
                     List.of(
-                            "http://localhost:5173",
-                            "http://localhost:5174",
-                            "http://localhost:5175"
+                            "http://localhost:*",
+                            "https://*.vercel.app"
                     )
             );
 
